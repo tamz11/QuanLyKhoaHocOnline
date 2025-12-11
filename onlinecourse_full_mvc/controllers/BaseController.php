8769b4822f5bf3880a2fa7fb7e587da02daf6e9b
tamz11 <mintam212005@gmail.com>
2025-12-11 23:55:45 +0700

    merge lại code

--- onlinecourse_full_mvc/controllers/BaseController.php
+++ onlinecourse_full_mvc/controllers/BaseController.php
@@ -32,8 +32,5 @@ abstract class BaseController {
             die("Bạn không có quyền truy cập trang này!");
         }
     }
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> ccf1cca4de56872d0fe79081a65aaa13f3aa7f09
